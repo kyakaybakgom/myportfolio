b2ec1e0c4ef57a22b1248ae3a6525113da4479cf
DESKTOP-L6H50I7\USER <pacgom85@gmail.com>
2024-09-10 23:47:51 +0900

    캐릭터 점프 버그 수정

--- MakeGameDev_RoadRun/Assets/Scripts/MainCharacterController.cs
+++ MakeGameDev_RoadRun/Assets/Scripts/MainCharacterController.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class MainCharacterController : MonoBehaviour {
     
     //string 변수 선언
-    private const string m_Vertical = "Vertical";
-    private const string m_Horizontal = "Horizontal";
-    private const string m_Blend = "Blend";
-    private const string m_Jump = "Jump";
+    private const string m_Vertical     = "Vertical";
+    private const string m_Horizontal   = "Horizontal";
+    private const string m_Blend        = "Blend";
+    private const string m_Jump         = "Jump";
 
-    private const string m_Plane = "Plane";
-    private const string m_Star = "STAR";
-    private const string m_Obstacle = "Obstacle";
+    private const string m_Plane        = "Plane";
+    private const string m_Star         = "STAR";
+    private const string m_Obstacle     = "Obstacle";
     
     Transform m_tranform;
     Rigidbody m_rigdbody;
@@ -20,13 +20,17 @@ public class MainCharacterController : MonoBehaviour {
     Animator m_anim;
     AudioSource m_audio;
 
-    bool isGrounded = true;
+    private DataManager dataManager = null;
 
     //speed
     float m_speed = 0.01f;
 
     //vertical, horizontal
     private float m_h = 0.0f;
+    
+    private readonly float  m_jumpForce         = 6.0f;     // 점프 속도
+
+    private bool isGrounded = true; // 캐릭터가 지면에 있는지 확인
 
     //SCORE
     int m_score = 10;
@@ -43,76 +47,91 @@ public class MainCharacterController : MonoBehaviour {
     //skill
     
 
-    //초기화
-    private void Awake()
+    
+    // Use this for initialization
+    void Start ()
+    {
+        Init();
+    }
+
+    // 초기화
+    void Init() 
     {
         m_tranform = this.transform;
         m_rigdbody = GetComponent<Rigidbody>();
         m_anim = GetComponent<Animator>();
         m_audio = GetComponent<AudioSource>();
         m_endingPanel.SetActive(false);
-    }
 
-    // Use this for initialization
-    void Start ()
-    {
-		
-	}
+        dataManager = dataManager ?? DataManager.Instance;
+
+        isGrounded = true;
+    }
 	
-	// Update is called once per frame
 	void Update ()
     {
-        //게임 리스타트
-        if (DataManager.Instance.GameAct.Equals(false))
+        if (dataManager.GameAct.Equals(true))
         {
+            //걷기
+            WalkFunc();
+
+            // 점프
+            JumpFunc();
+        }
+        else
+        {
+            //게임 리스타트
             if (Input.GetKeyDown(KeyCode.R))
             {
                 UnityEngine.SceneManagement.SceneManager.LoadScene(0);
             }
         }
+    }
 
-        if (DataManager.Instance.GameAct.Equals(false)) return;
-
-        //걷기
+    void WalkFunc()
+    {
         m_h = Input.GetAxis(m_Horizontal); //좌우
-        
-        m_tranform.position += Vector3.right * (m_h * m_speed);
 
-        //점프
+        m_tranform.position += Vector3.right * (m_h * m_speed);
+    }
+
+    void JumpFunc()
+    {
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded.Equals(true))
         {
             m_anim.CrossFade(m_Jump, 0.1f);
-            m_rigdbody.velocity += Vector3.up * 5.0f;
+
+            // 점프 높이에 따른 속도 계산
+            m_rigdbody.AddForce(Vector3.up * m_jumpForce, ForceMode.Impulse);
 
             m_audio.PlayOneShot(m_JumpAudio);
 
             isGrounded = false;
         }
-	}
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (DataManager.Instance.GameAct.Equals(false)) return;
-
-        if (other.CompareTag(m_Star))
-        {
-            DataManager.Instance.ScoreData += m_score;
-            m_scoreManager.ScoreUpdateFunc();
-        }
+        if (dataManager.GameAct.Equals(false)) return;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (DataManager.Instance.GameAct.Equals(false)) return;
+        if (collision.transform.CompareTag(m_Star))
+        {
+            dataManager.ScoreData += m_score;
+            m_scoreManager.ScoreUpdateFunc();
+        }
 
         if (collision.transform.CompareTag(m_Plane))
         {
-            isGrounded = true;
+            if (!isGrounded)
+                isGrounded = true;
         }
 
         if (collision.transform.CompareTag(m_Obstacle))
         {
-            DataManager.Instance.GameAct = false;
+            dataManager.GameAct = false;
             m_anim.CrossFade("Dead", 0.1f);
             m_audio.PlayOneShot(m_EndAudio);
 
